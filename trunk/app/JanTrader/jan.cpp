f1df3d4f5327a240fed089a72586752127b87d5e
albertogiannetti <albertogiannetti@02f44a22-f82b-0410-9188-a10df94407a9>
2008-06-25 22:48:43 +0000

    Fixed jan_matrix. Added historical data.

--- trunk/app/JanTrader/jan.cpp
+++ trunk/app/JanTrader/jan.cpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2007, Alberto Giannetti
+* Copyright (C) 2007,2008 A. Giannetti
 *
 * This file is part of Hudson.
 *
@@ -17,24 +17,15 @@
 * along with Hudson.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-// STDLIB
-#include <ctime>
-#include <cstdlib>
-#include <cmath>
-
-// IO
+// STD
 #include <iostream>
-#include <fstream>
 #include <string>
 
 // Boost
 #include <boost/date_time/gregorian/gregorian.hpp>
-#include <boost/program_options.hpp>
 
 // Hudson
-#include <EODDB.hpp>
-#include <EOMReturnFactors.hpp>
-#include <EOMReport.hpp>
+#include <Database.hpp>
 #include <PositionFactorsSet.hpp>
 #include <PositionsReport.hpp>
 
@@ -43,110 +34,50 @@
 
 using namespace std;
 using namespace boost::gregorian;
-
-namespace po = boost::program_options;
+using namespace Series;
 
 
 int main(int argc, char* argv[])
 {
-  string begin_date, end_date;
-  string long_dbfile, hedge_dbfile;
-  string long_symbol, hedge_symbol;
-  int entry_offset = 0;
-  int exit_offset = 0;
-
-  try {
-    po::options_description desc("Allowed options");
-    desc.add_options()
-      ("help", "produce help message")
-      ("long_dbfile", po::value<string>(&long_dbfile), "long price database")
-      ("hedge_dbfile", po::value<string>(&hedge_dbfile), "hedge price database")
-      ("long_symbol", po::value<string>(&long_symbol), "long ticker")
-      ("hedge_symbol", po::value<string>(&hedge_symbol), "hedge ticker")
-      ("begin_date", po::value<string>(&begin_date), "start of trading period (YYYY-MM-DD)")
-      ("end_date", po::value<string>(&end_date), "end of trading period (YYYY-MM-DD)")
-      ("entry_offset", po::value<int>(&entry_offset), "offset from canonical entry date")
-      ("exit_offset", po::value<int>(&exit_offset), "offset from canonical exit date")
-      ;
-
-    po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, desc), vm);
-    po::notify(vm);
-
-    if( vm.count("help") ) {
-      cout << desc << endl;
-      exit(EXIT_FAILURE);
-    }
-
-    if( vm["long_dbfile"].empty() || vm["hedge_dbfile"].empty() ||
-      	vm["long_symbol"].empty() || vm["hedge_symbol"].empty() ||
-	      vm["begin_date"].empty()  || vm["end_date"].empty() ) {
-      cout << desc << endl;
-      exit(EXIT_FAILURE);
-    }
-
-    cout << "Long db file: " << long_dbfile << endl;
-    cout << "Hedge db file: " << hedge_dbfile << endl;
-    cout << "Entry days offset: " << entry_offset << endl;
-    cout << "Exit days offset: " << exit_offset << endl;
-
-  } catch( exception& e) {
-    cerr << "Error: " << e.what() << endl;
-    exit(EXIT_FAILURE);
-  }
-
   try {
 
-    date load_begin(from_simple_string(begin_date));
-    if( load_begin.is_not_a_date() ) {
-      cerr << "Invalid begin date " << begin_date << endl;
-      exit(EXIT_FAILURE);
-    }
+    Database::SeriesFile sf;
+    Database::SERIES_MAP mSeries;
 
-    date load_end(from_simple_string(end_date));
-    if( load_end.is_not_a_date() ) {
-      cerr << "Invalid end date " << end_date << endl;
-      exit(EXIT_FAILURE);
-    }
+    // Short SPX
+    sf.filename = "../../db/SPX.csv";
+    sf.driver = EODDB::YAHOO;
+    mSeries.insert(Database::SERIES_MAP::value_type("SPX", sf));
 
-    cout << "Loading hedge db file " << hedge_dbfile << " from " << load_begin << " to " << load_end << "..." << endl;
-    Series::EODDB::instance().load(hedge_symbol, hedge_dbfile, Series::EODDB::YAHOO, load_begin, load_end);
+    // Long DFSCX
+    sf.filename = "../../db/DFSCX.csv";
+    sf.driver = EODDB::YAHOO;
+    mSeries.insert(Database::SERIES_MAP::value_type("DFSCX", sf));
 
-    cout << "Loading long db file " << long_dbfile << " from " << load_begin << " to " << load_end << "..." << endl;
-    Series::EODDB::instance().load(long_symbol, long_dbfile, Series::EODDB::YAHOO, load_begin, load_end);
-
-    const Series::EODSeries& long_db = Series::EODDB::instance().get(long_symbol);
-    const Series::EODSeries& hedge_db = Series::EODDB::instance().get(hedge_symbol);
-
-    cout << "Records: " << long_db.size() << endl;
-    cout << "Period: " << long_db.period() << endl;
-    cout << "Total days: " << long_db.days() << endl;
-
-    cout << "Hedge Records: " << hedge_db.size() << endl;
-    cout << "Hedge Period: " << hedge_db.period() << endl;
-    cout << "Hedge Total days: " << hedge_db.days() << endl;
+    // Load time series
+    Database db(date_period(date(1996, Jan, 1), date(2008, Jan, 31)), mSeries);
+    db.load();
+    db.print();
     
-    JanTrader trader(long_symbol, hedge_symbol);
-    trader.run(entry_offset, exit_offset); // canonical entry/exit dates (12/20 - 1/9)
+    // Run trading simulation
+    JanTrader trader("DFSCX", "SPX");
+    trader.run(0, 0); // canonical entry/exit dates (12/20 - 1/9)
 
-    /*
-    * Print closed positions
-    */
+    // Print closed positions
     Report::header("Closed trades");
-    trader.positions().stratPos().closed().print();
+    trader.positions().naturalPos().closed().print();
 
-    /*
-    * Print simulation reports
-    */
+    Report::header("Strategy trades");
+    trader.positions().stratPos().print();
+
+    // Print simulation reports
     Report::header("Trade results");
     ReturnFactors rf(trader.positions().stratPos());
     Report rp(rf);
     rp.print();
 
-    /*
-    * Position stats
-    */
-    Report::header("Positions stats");
+    // Print Position stats
+    Report::header("Position stats");
     PositionFactorsSet pf(trader.positions().stratPos().closed());
     PositionsReport pr(pf);
     pr.print();
--- trunk/app/JanTrader/jan_matrix.cpp
+++ trunk/app/JanTrader/jan_matrix.cpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2007, Alberto Giannetti
+* Copyright (C) 2007,2008 Alberto Giannetti
 *
 * This file is part of Hudson.
 *
@@ -17,149 +17,81 @@
 * along with Hudson.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-// Posix
-#include <ctime>
-#include <cstdlib>
-#include <cmath>
-
-// IO
+// STD
 #include <iostream>
-#include <fstream>
 #include <string>
 
 // Boost
 #include <boost/date_time/gregorian/gregorian.hpp>
-#include <boost/program_options.hpp>
 
-// Series
-#include "YahooDriver.hpp"
-#include "DaySeries.hpp"
-#include "DayPrice.hpp"
-#include "ReturnFactors.hpp"
-#include "Report.hpp"
+// Hudson
+#include <Database.hpp>
+#include <PositionFactorsSet.hpp>
+#include <Report.hpp>
+
+// App
 #include "JanTrader.hpp"
 
 using namespace std;
 using namespace boost::gregorian;
 using namespace Series;
 
-namespace po = boost::program_options;
-
-
-typedef Series::DaySeries<Series::DayPrice> DB;
-
 
 int main(int argc, char* argv[])
 {
-  string begin_date, end_date;
-  string long_dbfile, hedge_dbfile;
-  string long_symbol, hedge_symbol;
-  int entry_low_offset, entry_high_offset;
-  int exit_low_offset, exit_high_offset;
-
-  try {
-	po::options_description desc("Allowed options");
-	desc.add_options()
-	  ("help", "produce help message")
-	  ("long_dbfile", po::value<string>(&long_dbfile), "long price database")
-	  ("hedge_dbfile", po::value<string>(&hedge_dbfile), "hedge price database")
-	  ("long_symbol", po::value<string>(&long_symbol), "long ticker")
-	  ("hedge_symbol", po::value<string>(&hedge_symbol), "hedge ticker")
-	  ("begin_date", po::value<string>(&begin_date), "start of trading period (YYYY-MM-DD)")
-	  ("end_date", po::value<string>(&end_date), "end of trading period (YYYY-MM-DD)")
-	  ("entry_low_offset", po::value<int>(&entry_low_offset)->default_value(-5), "entry low offset")
-	  ("entry_high_offset", po::value<int>(&entry_high_offset)->default_value(5), "entry high offset")
-	  ("exit_low_offset", po::value<int>(&exit_low_offset)->default_value(-5), "exit low offset")
-	  ("exit_high_offset", po::value<int>(&exit_high_offset)->default_value(5), "exit high offset")
-	  ;
-
-	po::variables_map vm;
-	po::store(po::parse_command_line(argc, argv, desc), vm);
-	po::notify(vm);
-
-	if( vm.count("help") ) {
-	  cout << desc << endl;
-	  exit(EXIT_FAILURE);
-	}
-
-	if( vm["long_dbfile"].empty() || vm["hedge_dbfile"].empty() ||
-		vm["long_symbol"].empty() || vm["hedge_symbol"].empty() ||
-		vm["begin_date"].empty()  || vm["end_date"].empty() ) {
-	  cout << desc << endl;
-	  exit(EXIT_FAILURE);
-	}
-
-	cout << "Long db file: " << long_dbfile << endl;
-	cout << "Hedge db file: " << hedge_dbfile << endl;
-
-  } catch( exception& e) {
-	cerr << "Error: " << e.what() << endl;
-	exit(EXIT_FAILURE);
-  }
-
-  YahooDriver yd;
-  DB long_db(long_symbol, yd);
-  DB hedge_db(hedge_symbol, yd);
-
   try {
 
-	date load_begin(from_simple_string(begin_date));
-	if( load_begin.is_not_a_date() ) {
-	  cerr << "Invalid begin date " << begin_date << endl;
-	  exit(EXIT_FAILURE);
-	}
+    Database::SeriesFile sf;
+    Database::SERIES_MAP mSeries;
 
-	date load_end(from_simple_string(end_date));
-	if( load_end.is_not_a_date() ) {
-	  cerr << "Invalid end date " << end_date << endl;
-	  exit(EXIT_FAILURE);
-	}
+    // Short SPX
+    sf.filename = "../../db/SPX.csv";
+    sf.driver = EODDB::YAHOO;
+    mSeries.insert(Database::SERIES_MAP::value_type("SPX", sf));
 
-	cout << "Loading hedge db file " << hedge_dbfile << " from " << load_begin << " to " << load_end << "..." << endl;
-	if( hedge_db.load(hedge_dbfile, load_begin, load_end) <= 0) {
-	  cerr << "No records found in " << hedge_dbfile << endl;
-	  exit(EXIT_FAILURE);
-	}
+    // Long DFSCX
+    sf.filename = "../../db/DFSCX.csv";
+    sf.driver = EODDB::YAHOO;
+    mSeries.insert(Database::SERIES_MAP::value_type("DFSCX", sf));
 
-	cout << "Loading long db file " << long_dbfile << " from " << load_begin << " to " << load_end << "..." << endl;
-	if( long_db.load(long_dbfile, load_begin, load_end) <= 0 ) {
-	  cerr << "No records found in" << long_dbfile << endl;
-	  exit(EXIT_FAILURE);
-	}
+    // Load time series
+    Database db(date_period(date(1996, Jan, 1), date(2008, Jan, 31)), mSeries);
+    db.load();
+    db.print();
+    
+    const int entry_low_offset = -3;
+    const int entry_high_offset = 3;
+    const int exit_low_offset = -3;
+    const int exit_high_offset = 3;
+    cout << "Entry, Exit, Avg, Std Dev, Pos, Neg, % Pos, % Neg, Avg Pos, Avg Neg, Best, Worst" << endl;
+    cout.precision(4);
+    for( int i = entry_low_offset; i <= entry_high_offset; i++ ) {
+      for( int j = exit_low_offset; j <= exit_high_offset; j++ ) {
 
-  } catch( std::out_of_range& e ) {
-	cerr << "Can't get begin/end dates: " << e.what() << endl;
-	exit(EXIT_FAILURE);
+        JanTrader trader("DFSCX", "SPX");
+        trader.run(i, j);
 
-  } catch( Series::DriverException& e ) {
-	cerr << "Driver error: " << e.what() << endl;
-	exit(EXIT_FAILURE);
-  }
+        ReturnFactors rf(trader.positions().stratPos());
+        ReturnFactors rf_pos(rf.pos());
+        ReturnFactors rf_neg(rf.neg());
 
-  JanTrader trader(long_db, hedge_db);
+        double percent_pos = rf_pos.num() / rf.num();
+        double percent_neg = rf_neg.num() / rf.num();
 
-  cout.precision(4);
+        cout << i << ',' << j << ',';
+        cout << rf.avg() << ',' << rf.stddev() << ',';
+        cout << rf_pos.num() << ',' << rf_neg.num() << ',';
+        cout << percent_pos << ',' << percent_neg << ',';
+        cout << rf_pos.avg() << ',' << rf_neg.avg() << ',';
+        cout << rf.best().factor()-1 << ',' << rf.worst().factor()-1;
+        cout << endl;
+      }
+    }
 
-  cout << "Entry, Exit, Avg, Std Dev, Pos, Neg, % Pos, % Neg, Avg Pos, Avg Neg, Best, Worst, Cons Pos, Cons Neg" << endl;
-  for( int i = entry_low_offset; i <= entry_high_offset; i++ ) {
-	for( int j = exit_low_offset; j <= exit_high_offset; j++ ) {
+  } catch( std::exception& ex ) {
 
-	  trader.run(i, j);
-
-	  ReturnFactors rf(trader.factors(), long_db.duration().days(), 12);
-	  ReturnFactors rf_pos(rf.pos(), long_db.duration().days(), 12);
-	  ReturnFactors rf_neg(rf.neg(), long_db.duration().days(), 12);
-
-	  double pos_percent = rf_pos.num() / (double)rf.num();
-	  double neg_percent = rf_neg.num() / (double)rf.num();
-
-	  cout << i << ',' << j << ',';
-	  cout << rf.avg() << ',' << rf.stddev() << ',';
-	  cout << rf_pos.num() << ',' << rf_neg.num() << ',' << pos_percent << ',' << neg_percent << ',';
-	  cout << rf_pos.avg() << ',' << rf_neg.avg() << ',';
-	  cout << rf.best() << ',' << rf.worst() << ',' << rf.max_cons_pos() << ',' << rf.max_cons_neg() << ',';
-	  cout << endl;
-	}
+    cerr << "Unhandled exception: " << ex.what() << endl;
+    exit(-1);
   }
 
   return 0;
